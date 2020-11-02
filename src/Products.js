0193d89b25264d447b5a3aa843038fcf667aa00f
bwyard <bwyard@gmail.com>
2020-11-02 11:08:20 -0600

    added product property

--- src/Products.js
+++ src/Products.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 function Products(){
     const products =["Learning React","Pro React","Beginning React"];
+    const listProducts = products.map((product) => <li key = {product.toString()}>{product}</li>)
     return (
         <div>
             <h1>
