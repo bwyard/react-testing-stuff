37950ae2b3ac8402afd284ae770904e87b1941d9
bwyard <bwyard@gmail.com>
2020-11-02 11:28:00 -0600

    more props stuff??

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
 import Products from './Products'
 import {Button} from 'react-bootstrap';
+import Rating from './Rating';
 
 function App() {
   return (
     <div className="App">
-      <h1>
-        Learn React Hooks
-        <Products />
-        <Button variant="danger">Default</Button>
-      </h1>
+      <Rating rating ='1' />
+      <Rating rating ='2' />
+      <Rating rating ='3' />
+      <Rating rating ='4' />
+      <Rating rating ='5' />
     </div>
   );
 }
