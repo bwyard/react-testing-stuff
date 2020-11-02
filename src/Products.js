4abaf01f584de722f613866ea25bf47a36e846c4
bwyard <bwyard@gmail.com>
2020-11-02 11:06:25 -0600

    following book on hooks

---
+++ src/Products.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Products(){
+    const products =["Learning React","Pro React","Beginning React"];
+    return (
+        <div>
+            <h1>
+                Products
+            </h1>
+        </div>
+    );
+}
+
+export default Products;
