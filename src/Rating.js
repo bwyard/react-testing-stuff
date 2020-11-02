bcaabce622859b9e8192d51c263fef0ec82a1529
bwyard <bwyard@gmail.com>
2020-11-02 12:04:19 -0600

    onClick

--- src/Rating.js
+++ src/Rating.js
@@ -6,11 +6,11 @@ function Rating(props) {
     return(
         <div>
             <h1> Rating: {rating}</h1>
-            {rating >=1?(<IoIosStar/>):(<IoIosStarOutline/>)}
-            {rating >=2?(<IoIosStar/>):(<IoIosStarOutline/>)}
-            {rating >=3?(<IoIosStar/>):(<IoIosStarOutline/>)}
-            {rating >=4?(<IoIosStar/>):(<IoIosStarOutline/>)}
-            {rating >=5?(<IoIosStar/>):(<IoIosStarOutline/>)}
+            {rating >=1?(<IoIosStar onClick={()=>setRating(1)}/>):(<IoIosStarOutline onClick={()=>setRating(1)}/>)}
+            {rating >=2?(<IoIosStar onClick={()=>setRating(2)}/>):(<IoIosStarOutline onClick={()=>setRating(2)}/>)}
+            {rating >=3?(<IoIosStar onClick={()=>setRating(3)}/>):(<IoIosStarOutline onClick={()=>setRating(3)}/>)}
+            {rating >=4?(<IoIosStar onClick={()=>setRating(4)}/>):(<IoIosStarOutline onClick={()=>setRating(4)}/>)}
+            {rating >=5?(<IoIosStar onClick={()=>setRating(5)}/>):(<IoIosStarOutline onClick={()=>setRating(5)}/>)}
         </div>
     )
 }
