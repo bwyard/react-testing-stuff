0df6d10b4fde8eebf097c39cbbb6bc421be3ee30
bwyard <bwyard@gmail.com>
2020-11-03 12:33:24 -0600

    user validation

--- src/UserForm.js
+++ src/UserForm.js
@@ -4,9 +4,33 @@ import {Form, Button} from 'react-bootstrap'
 function UserForm(){
     const [email,setEmail] =useState("")
     const [password,setPassword] = useState("")
+    const [emailError, setEmailError] = useState("")
+    const [passwordError,setPasswordError] =useState("")
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        var emailValid = false;
+        if(email.length ===0) {
+            setEmailError("email is required");
+        }
+        else if(email.length <6){
+            setEmailError("Email should be miniminum 6 characters")
+        }
+        else if(email.includes(" ")){
+            setEmailError("Email cannot contain spaces");
+        }
+        else{
+            setEmailError("")
+            emailValid = true
+        }
+
+        if(emailValid){
+            alert(`Email: ${email}\nPassword: ${password}`)
+        }
+    }
     return(
         <div>
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <Form.Group controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
                     <Form.Control type="email" placeholder="Enter email" 
