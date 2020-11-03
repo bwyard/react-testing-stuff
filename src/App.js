60e1b406d3ff0a83b5819f3a6829915d014bcc05
bwyard <bwyard@gmail.com>
2020-11-03 12:33:31 -0600

    updatyed app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
 import Products from './Products'
-import {Button} from 'react-bootstrap';
-import Rating from './Rating';
+import UserForm from './UserForm'
 
 function App() {
   return (
     <div className="App">
-      <Products />
+      <UserForm />
     </div>
   );
 }
